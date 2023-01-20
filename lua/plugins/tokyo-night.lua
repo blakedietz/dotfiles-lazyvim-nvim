418c240e92f77b2d175f72926c6ebd2b66cfcf3e
blakedietz <blakedietz@gmail.com>
2023-01-20 11:01:21 -0700

    feat(colorscheme): update border separtors have more contrast

---
+++ lua/plugins/tokyo-night.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      dim_inactive = true,
+      on_highlights = function(hl, c)
+        -- Make borders slightly more legible VertSplit didn't seem to do anything
+        hl.WinSeparator = {
+          fg = c.cyan,
+        }
+      end,
+    },
+  },
+}
