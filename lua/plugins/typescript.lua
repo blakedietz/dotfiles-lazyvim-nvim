4e71e91ebf4d1544b6ffff3ef21bd216e954fdd4
blakedietz <blakedietz@gmail.com>
2023-01-24 09:02:23 -0800

    add lazyvim configs for typescript

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,6 @@
+return {
+  -- Taken from the example.lua
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+}
