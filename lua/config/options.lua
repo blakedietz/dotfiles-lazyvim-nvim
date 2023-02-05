88fc057162edd048818764eab80dfb7d38a2faf7
blakedietz <blakedietz@gmail.com>
2023-02-05 12:35:55 -0700

    feat(config): add whitespace characters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show all whitespace
+vim.o.list = true
+vim.o.lcs = "tab:> ,trail:·,eol:¬,nbsp:_"
