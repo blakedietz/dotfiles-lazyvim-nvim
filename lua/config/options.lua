4607703203823738f2042ff3fdd0725fc56cff5b
blakedietz <blakedietz@gmail.com>
2023-07-08 12:34:44 -0600

    update support for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,8 @@
 -- Show all whitespace
 vim.o.list = true
 vim.o.lcs = "tab:> ,trail:·,eol:¬,nbsp:_"
+
+if vim.g.neovide then
+  vim.o.guifont = "BerkeleyMono Nerd Font:h16"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
