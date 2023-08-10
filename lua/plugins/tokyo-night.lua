069e3f9329daf29d732e82438b84a21a6c23e769
blakedietz <blakedietz@gmail.com>
2023-08-10 13:44:30 -0600

    refactor(color scheme): add check for vscode
    Disable the color scheme when vscode is using neovim in a headless mode to stop the colorschemes from conflicting.

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -1,14 +1,25 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      dim_inactive = true,
-      on_highlights = function(hl, c)
-        -- Make borders slightly more legible VertSplit didn't seem to do anything
-        hl.WinSeparator = {
-          fg = c.cyan,
-        }
-      end,
+if vim.g.vscode then
+  return {
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "",
+      },
     },
-  },
-}
+  }
+else
+  return {
+    {
+      "folke/tokyonight.nvim",
+      opts = {
+        dim_inactive = true,
+        on_highlights = function(hl, c)
+          -- Make borders slightly more legible VertSplit didn't seem to do anything
+          hl.WinSeparator = {
+            fg = c.cyan,
+          }
+        end,
+      },
+    },
+  }
+end
